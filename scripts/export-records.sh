e1dd17be7065b633f8762554689cf984dbe11cdd
Andrei Jiroh Halili (RecapTime.dev) <ajhalili2006@crew.recaptime.dev>
2024-11-21 01:53:22 +0800

    chore(scripts): ship some scripts and then some
    Signed-off-by: Andrei Jiroh Halili (RecapTime.dev) <ajhalili2006@crew.recaptime.dev>

--- scripts/export-records.sh
+++ scripts/export-records.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ -z "${VIRTUAL_ENV}" ]]; then
+  echo "You're outside your virtualenv, proceed at your own risk".
+fi
+
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
 if [ -f "$GIT_ROOT/.env.keys" ]; then
   export $(grep -v '^#' .env.keys | xargs)
 fi
 
-dotenvx run -f "${GIT_ROOT}/.env.ci" -- octodns-dump --config-file octodns-config.yml --output-dir=dns recaptime.dev. cf-prod
-dotenvx run -f "${GIT_ROOT}/.env.ci" -- octodns-dump --config-file octodns-config.yml --output-dir=dns lorebooks.eu.org. cf-prod
-dotenvx run -f "${GIT_ROOT}/.env.ci" -- octodns-dump --config-file octodns-config.yml --output-dir=dns recaptime.eu.org. cf-prod
+dotenvx run -f .env.ci -- octodns-dump --config-file octodns-config.yml --output-dir=dns '*' cf-prod
